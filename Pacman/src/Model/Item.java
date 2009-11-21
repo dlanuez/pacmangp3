73cd46ceb086c523f30912c0b99b5071c0871019
gabrielalejandrocava <gabrielalejandrocava@270c561a-cfd4-11de-9e88-ff79fe1e0c1e>
2009-11-21 17:37:29 +0000

    Primera implementacion de la clase Bolita, Fruta y Pastilla(falta que cambie de estado a los vivientes). Modifique un par de cosas de Item y Casillero. Agregue la clase Jugador y agregue el atributo jugador a la clase Juego.

--- Pacman/src/Model/Item.java
+++ Pacman/src/Model/Item.java
@@ -5,7 +5,24 @@ public abstract class Item {
 	private Juego juego;
 	 
 	private int puntosOtorgados;
-	 
+	
+	protected void setJuego(Juego juego){
+		this.juego = juego;
+	}
+	
+	protected Juego getJuego(){
+		return this.juego;
+	}
+	
+	protected void setPuntosOtorgados(int puntos){
+		this.puntosOtorgados = puntos;
+	}
+	
+	protected int getPuntosOtorgados(){
+		return this.puntosOtorgados;
+	}
+		
+	
 	public void hacerEfecto() {
 	 
 	}
