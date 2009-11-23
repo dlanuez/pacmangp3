899005582ee21f4290a07d49a9542e946ee230ea
gabrielalejandrocava <gabrielalejandrocava@270c561a-cfd4-11de-9e88-ff79fe1e0c1e>
2009-11-23 19:31:42 +0000

    Los test de pastilla y casillero funcionan. Agregue la inicializacion de las velocidades de los fantasmas en cada clase de fantasma. Y tambien la inicializacion del estado de los fantasmas en la clase fantasma. Y sus respectivas excepciones.

--- Pacman/src/Model/FantasmaRojo.java
+++ Pacman/src/Model/FantasmaRojo.java
@@ -1,11 +1,13 @@
 package Model;
 
 import Model.excepciones.PosicionInvalidaException;
+import Model.excepciones.VelocidadInvalidaException;
 
 public class FantasmaRojo extends Fantasma {
 
-	public FantasmaRojo(Punto posicionInicial, Juego juego) throws PosicionInvalidaException {
+	public FantasmaRojo(Punto posicionInicial, Juego juego) throws PosicionInvalidaException, VelocidadInvalidaException {
 		super(posicionInicial, juego);
+		this.setVelocidad(1); //TODO poner una velocidad real.
 	}
 
 }
