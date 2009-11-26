cbd4be00d731b39edeed35db5208a3dd3b3649ec
canuhedc <canuhedc@270c561a-cfd4-11de-9e88-ff79fe1e0c1e>
2009-11-26 18:09:45 +0000

    agregada asignacion de estrategias a los fantasmas

--- trunk/Pacman/src/Model/FantasmaRosa.java
+++ trunk/Pacman/src/Model/FantasmaRosa.java
@@ -8,6 +8,7 @@ public class FantasmaRosa extends Fantasma {
 	public FantasmaRosa(Punto posicionInicial, Juego juego) throws PosicionInvalidaException, VelocidadInvalidaException {
 		super(posicionInicial, juego);
 		this.setVelocidad(1); //TODO poner una velocidad real.
+		this.setEstrategia(new EstrategiaPerseguidora());
 	}
 
 }
--- trunk/Pacman/src/Model/FantasmaVerde.java
+++ trunk/Pacman/src/Model/FantasmaVerde.java
@@ -10,6 +10,7 @@ public class FantasmaVerde extends Fantasma {
 	public FantasmaVerde(Punto posicionInicial, Juego juego) throws PosicionInvalidaException, VelocidadInvalidaException {
 		super(posicionInicial, juego);
 		this.setVelocidad(1); //TODO poner una velocidad real.
+		this.setEstrategia(new EstrategiaPerseguidora());
 	}
 	
 	@Override
