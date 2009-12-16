259079371711ec40fea0bd13f74942182dc016d9
gabrielalejandrocava <gabrielalejandrocava@270c561a-cfd4-11de-9e88-ff79fe1e0c1e>
2009-12-16 19:02:17 +0000

    Test de Bolita y Fruta.

---
+++ Pacman/test/BolitaTest.java
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException;
+
+import Model.item.Bolita;
+import Model.juego.Juego;
+import junit.framework.TestCase;
+
+
+public class BolitaTest extends TestCase {
+
+	private Bolita bolita;
+	private Juego juego;
+	
+	public void testHacerEfectoOK(){
+		this.juego = new Juego("src/Model/nivel1.xml", 16, 16);
+		try {
+			this.juego.getTablero().inicializar();
+		} catch (FileNotFoundException e1) {
+			fail("Arrojo Excepcion FileNotFound");
+		}
+		this.bolita = new Bolita(this.juego);
+		int puntosAntes = this.juego.getJugador().getPuntos();
+		this.bolita.hacerEfecto();
+		assertEquals(this.juego.getJugador().getPuntos(), puntosAntes+10);	
+	}
+	
+	public void testHacerEfectoErroneo(){
+		this.juego = new Juego("src/Model/nivel1.xml", 16, 16);
+		try {
+			this.juego.getTablero().inicializar();
+		} catch (FileNotFoundException e1) {
+			fail("Arrojo Excepcion FileNotFound");
+		}
+		this.bolita = new Bolita(this.juego);
+		int puntosAntes = this.juego.getJugador().getPuntos();
+		this.bolita.hacerEfecto();
+		assertFalse(this.juego.getJugador().getPuntos() == puntosAntes);
+	}
+
+}
