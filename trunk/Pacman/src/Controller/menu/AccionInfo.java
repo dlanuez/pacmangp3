3a1b3b29d7cf549fa5545275de8d6dc399727369
juampiarnaudo <juampiarnaudo@270c561a-cfd4-11de-9e88-ff79fe1e0c1e>
2009-12-17 06:22:13 +0000

    Info con info posta y un poquito más de nivel ;)

--- trunk/Pacman/src/Controller/menu/AccionInfo.java
+++ trunk/Pacman/src/Controller/menu/AccionInfo.java
@@ -8,8 +8,16 @@ import javax.swing.JOptionPane;
 public class AccionInfo implements ActionListener {
 
 	public void actionPerformed(ActionEvent ae) {
-		JOptionPane.showMessageDialog(null, "Trabajo Practico Algoritmos y Programacion III", "Algo3-Man", 1);
+		JOptionPane.showMessageDialog(null,
+				"Trabajo Practico Algoritmos y Programacion III\n" + "\n"
+				+ "Integrantes: \n"
+				+ "		Selvanovich Elias \n"
+				+ "		Arnaudo Juan Pablo \n"
+				+ "		Canuhe Diego \n"
+				+ "		Cava Gabriel Alejandro"
+				
+				
+				, "Algo3-Man", 1);
 	}
 		 
 }
-
